fc35fae4450e3ad936e468325b039302d0420510
Christian Mauduit <ufoot@ufoot.org>
2012-01-23 20:03:00 +0100

    Fixed liquidwar-configure.sh utility to match recent repository cleanup

--- liquidwar6/misc/liquidwar6-configure.sh
+++ liquidwar6/misc/liquidwar6-configure.sh
@@ -4,7 +4,11 @@ BUILDDIR=/mnt/ramdisk/liquidwar6
 SRCDIR=$HOME/Home/_/liquidwar6
 INSTALLDIR=$HOME/Home/local/liquidwar6
 
-if test -x $SRCDIR/configure ; then
+if test -e $SRCDIR/configure.ac ; then
+	cd $SRCDIR
+	aclocal -I m4 --install
+	automake
+	autoconf
 	install -d $BUILDDIR
 	cd $BUILDDIR
 	$SRCDIR/configure --enable-profiler --prefix=$INSTALLDIR
