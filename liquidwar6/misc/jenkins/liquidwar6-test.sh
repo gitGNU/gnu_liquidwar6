d03ea9baa2d3785618c8f0454b90c71e709b4337
Christian Mauduit <ufoot@ufoot.org>
2013-07-11 10:55:33 +0200

    Checking fo Xvfb existence

--- liquidwar6/misc/jenkins/liquidwar6-test.sh
+++ liquidwar6/misc/jenkins/liquidwar6-test.sh
@@ -25,15 +25,15 @@
 
 if test x$WORKSPACE = x ; then
     if test x$TMP = x ; then
-	if test x$TMPDIR = x ; then
-	    WORKSPACE=/tmp
-	else
-	    WORKSPACE=$TMPDIR
-	fi
+        if test x$TMPDIR = x ; then
+            WORKSPACE=/tmp
+        else
+            WORKSPACE=$TMPDIR
+        fi
     else
-	WORKSPACE=$TMP
+        WORKSPACE=$TMP
     fi
-fi	
+fi      
 
 # This will build and check the program, it runs it a quite
 # paranoid mode, running a check then a dist then a distcheck.
@@ -43,50 +43,52 @@ echo "******** $0 $(date) ********"
 if cd liquidwar6 ; then
     echo "******** $0 $(date) ********"
     if autoreconf ; then
-	echo "******** $0 $(date) ********"
-	if ./configure --prefix=$WORKSPACE/local ; then
-	    echo "******** $0 $(date) ********"
-	    if make ; then
-		echo "******** $0 $(date) ********"
-		killall Xvfb
-		Xvfb :6 -pixdepths 1 8 16 24 32 -screen 0 640x480x24 &
-		export DISPLAY=:6
-		echo "******** $0 $(date) ********"
-		# No check on --cunit output, make check will do this, we do not
-		# want the whole build to fail just because of cunit, only, we
-		# look at the XML output with Jenkins
-		src/liquidwar6 --test
-		killall Xvfb
-		if [ -f $HOME/.liquidwar6/CUnit-Results.xml ] && [ -x /usr/bin/xsltproc ] && [ -x /usr/bin/xmllint ] ; then
-		    rm -f *-Results.xml
-		    xmllint --recover $HOME/.liquidwar6/CUnit-Results.xml > CUnit-Results.xml
-		    /usr/bin/xsltproc --output JUnit-Results.xml --path ./misc/cunit-to-junit/ misc/cunit-to-junit/cunit-to-junit.xsl CUnit-Results.xml
-		fi
-		echo "******** $0 $(date) ********"
-		if make distcheck; then
-		    echo "******** $0 $(date) ********"
-		else
-		    killall Xvfb
-		    echo "make distcheck failed"
-		    exit 5
-		fi
-	    else
-		killall Xvfb
-		echo "make failed"
-		exit 4
-	    fi
-	else
-	    killall Xvfb
-	    echo "./configure failed"
-	    exit 3
-	fi
+        echo "******** $0 $(date) ********"
+        if ./configure --prefix=$WORKSPACE/local ; then
+            echo "******** $0 $(date) ********"
+            if make ; then
+                if [ -x /usr/bin/Xvfb ] ; then
+                    echo "******** $0 $(date) ********"
+                    killall Xvfb
+                    Xvfb :6 -pixdepths 1 8 16 24 32 -screen 0 640x480x24 &
+                    export DISPLAY=:6
+                    echo "******** $0 $(date) ********"
+                    # No check on --cunit output, make check will do this, we do not
+                    # want the whole build to fail just because of cunit, only, we
+                    # look at the XML output with Jenkins
+                    src/liquidwar6 --test
+                    killall Xvfb
+                    if [ -f $HOME/.liquidwar6/CUnit-Results.xml ] && [ -x /usr/bin/xsltproc ] && [ -x /usr/bin/xmllint ] ; then
+                        rm -f *-Results.xml
+                        xmllint --recover $HOME/.liquidwar6/CUnit-Results.xml > CUnit-Results.xml
+                        /usr/bin/xsltproc --output JUnit-Results.xml --path ./misc/cunit-to-junit/ misc/cunit-to-junit/cunit-to-junit.xsl CUnit-Results.xml
+                    fi
+                fi
+                echo "******** $0 $(date) ********"
+                if make distcheck; then
+                    echo "******** $0 $(date) ********"
+                else
+                    killall Xvfb
+                    echo "make distcheck failed"
+                    exit 5
+                fi
+            else
+                killall Xvfb
+                echo "make failed"
+                exit 4
+            fi
+        else
+            killall Xvfb
+            echo "./configure failed"
+            exit 3
+        fi
     else
-	killall Xvfb
-	echo "autoreconf failed"
-	exit 2
+        killall Xvfb
+        echo "autoreconf failed"
+        exit 2
     fi
 else
-echo "cd failed"
+    echo "cd failed"
     exit 1
 fi
 
