fc302dfa1fe4a94e70978febaab4e448ffd6cc32
Christian Mauduit <ufoot@ufoot.org>
2013-10-15 01:04:16 +0200

    Added MSYS dedicated Jenkins script.

---
+++ liquidwar6/misc/jenkins/liquidwar6-msys.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# Liquid War 6 is a unique multiplayer wargame.
+# Copyright (C)  2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013  Christian Mauduit <ufoot@ufoot.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Liquid War 6 homepage : http://www.gnu.org/software/liquidwar6/
+# Contact author        : ufoot@ufoot.org
+
+# Script used by Jenkins daemon to autobuild the program in
+# continuous integration mode. http://jenkins-ci.org/
+
+if test x$WORKSPACE = x ; then
+    if test x$TMP = x ; then
+        if test x$TMPDIR = x ; then
+            WORKSPACE=/tmp
+        else
+            WORKSPACE=$TMPDIR
+        fi
+    else
+        WORKSPACE=$TMP
+    fi
+fi      
+
+# This will build and check the program in an msys environment,
+# it does not perform as many checks since msys is sort of...
+# limited, but still it does at least compile the binary
+
+echo "******** $0 $(date) ********"
+git clean -d -f -x
+if cd liquidwar6 && rm -f *.gz doc/*.gz pkg/*.gz pkg/*vendor* ; then
+    echo "cd liquidwar6 OK"
+else
+    echo "cd liquidwar6 failed"
+    exit 1
+fi
+
+echo "******** $0 $(date) ********"
+if ./configure --prefix=$WORKSPACE/local ; then
+    echo "./configure OK"
+else
+    echo "./configure failed"
+    exit 2
+fi
+
+echo "******** $0 $(date) ********"
+if make clean && make ; then
+    echo "make OK"
+else
+    echo "make failed"
+    exit 3
+fi
+
+echo "******** $0 $(date) ********"
+echo "OK"
+exit 0
