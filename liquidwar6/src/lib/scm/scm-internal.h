c09c7d1c6e3ffb01770b3c58cfeacbc2beb9911d
Christian Mauduit <ufoot@ufoot.org>
2014-05-30 11:19:09 +0200

    sys-context refactoring, fixed libscm

---
+++ liquidwar6/src/lib/scm/scm-internal.h
@@ -0,0 +1,38 @@
+/*
+  Liquid War 6 is a unique multiplayer wargame.
+  Copyright (C)  2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014  Christian Mauduit <ufoot@ufoot.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+  Liquid War 6 homepage : http://www.gnu.org/software/liquidwar6/
+  Contact author        : ufoot@ufoot.org
+*/
+
+#ifndef LIQUIDWAR6SCM_INTERNAL_H
+#define LIQUIDWAR6SCM_INTERNAL_H
+
+#include "scm.h"
+
+typedef struct _lw6scm_wrapper_callback_data_s
+{
+  lw6sys_context_t *sys_context;
+  lw6scm_callback_t func;
+  void *data;
+} _lw6scm_wrapper_callback_data_t;
+
+/* scm-wrapper.c */
+extern void *_lw6scm_wrapper_callback_func (void *data);
+
+#endif // LIQUIDWAR6SCM_INTERNAL_H
